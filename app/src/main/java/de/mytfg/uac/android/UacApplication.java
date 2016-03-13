0ba60d248b8c69df1b7be5c47a474af404862b31
Raphael Lehmann <raphael@rleh.de>
2016-03-13 21:29:50 +0100

    initial commit

---
+++ app/src/main/java/de/mytfg/uac/android/UacApplication.java
@@ -0,0 +1,33 @@
+package de.mytfg.uac.android;
+
+import android.app.Application;
+
+import org.acra.ACRA;
+import org.acra.ReportingInteractionMode;
+import org.acra.annotation.ReportsCrashes;
+import org.acra.sender.HttpSender;
+
+
+@ReportsCrashes(
+        httpMethod = HttpSender.Method.PUT,
+        reportType = HttpSender.Type.JSON,
+        formUri = "https://acra.mytfg.de/acra-adventskalender/_design/acra-storage/_update/report",
+        formUriBasicAuthLogin = "adventskalender_app",
+        formUriBasicAuthPassword = "1hv8AeshKfgWyKtbsJxM420La8iLcRUlEg==",
+
+        mode = ReportingInteractionMode.TOAST,
+        resToastText = R.string.crash_toast
+)
+
+public class UacApplication extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        // Initialise ACRA:
+        ACRA.init(this);
+    }
+
+}
+
